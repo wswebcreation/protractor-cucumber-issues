88043803152275c3d9a10c4e766761e20add4a01
Wim Selles <wim@wswebcreation.nl>
2017-09-12 08:58:55 +0200

    chore: initial commit for version-2a

--- test/steps.js
+++ test/steps.js
@@ -1,5 +1,7 @@
-module.exports = function () {
-    this.Given(/^I (foo|bar)$/, (fooBar) => Promise.resolve(`I ${fooBar}`));
-    this.When(/^I (foo|bar) it$/, (fooBar) => Promise.resolve(`I ${fooBar} it`));
-    this.Then(/^I (foo|bar) till I (foo|bar)$/, (fooBar, barFoo) => Promise.resolve(`I ${fooBar} till I ${barFoo}`));
-};
+const {defineSupportCode} = require('cucumber');
+
+defineSupportCode(({Given, When, Then}) => {
+    Given(/^I (foo|bar)$/, (fooBar) => Promise.resolve(`I ${fooBar}`));
+    When(/^I (foo|bar) it$/, (fooBar) => Promise.resolve(`I ${fooBar} it`));
+    Then(/^I (foo|bar) till I (foo|bar)$/, (fooBar, barFoo) => Promise.resolve(`I ${fooBar} till I ${barFoo}`));
+});
